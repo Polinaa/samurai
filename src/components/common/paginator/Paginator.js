1dbee41106f09e2abdb2959971074afe79f1a2d9
Polina Kvachuk <pulliia@Polinas-MacBook-Pro.local>
2020-06-04 23:08:52 +0300

    93 update pagination

--- src/components/common/paginator/Paginator.js
+++ src/components/common/paginator/Paginator.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Paginator.module.css'
 
-let Paginator = (props) => {
-    let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
+let Paginator = ({currentPage, totalItemsCount, pageSize, onPageChanged}) => {
+    let pageCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 0; i < pageCount; i++) {
         pages.push(i + 1);
     }
+    let portionSize = 10;
+    let [currentPageStart, setCurrentPageStart] = useState(currentPage - (currentPage - 1) % portionSize);
+
     let pageElements = pages
-        .filter(p => p < 12)
+        .filter(p => p >= currentPageStart && p < currentPageStart + portionSize)
         .map(p => {
                 return (<span key={p}
-                              className={p === props.currentPage ? s.selectedPage : ""}
+                              className={p === currentPage ? s.selectedPage : ""}
                               onClick={() => {
-                                  props.onPageChanged(p)
+                                  onPageChanged(p)
                               }}>{p} </span>);
             }
         );
     return (
         <div>
+            {currentPageStart > 1
+            && <button onClick={() => setCurrentPageStart(currentPageStart - portionSize)}>Prev</button>}
             {pageElements}
+            {currentPageStart + portionSize - 1 < totalItemsCount
+            && <button onClick={() => setCurrentPageStart(currentPageStart + portionSize)}>Next</button>}
         </div>
     )
 }
